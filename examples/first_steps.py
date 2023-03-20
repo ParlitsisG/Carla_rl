7495c50b7e18e746cca94608a3900d61bceaadc6
ParlitsisG <parlitsis.g@gmail.com>
2023-03-20 19:20:36 +0200

    Update first_steps.py
    spawn_npc_crashing

--- examples/first_steps.py
+++ examples/first_steps.py
@@ -24,8 +24,8 @@ try:
         sys.version_info.major,
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
-    sys.path.append('C:/Users/parli/Desktop/CARLA_0.9.8/WindowsNoEditor/PythonAPI/carla')
-    from agents.navigation.local_planner import LocalPlanner
+    sys.path.append('C:/Users/parli/Desktop/CARLA_0.9.8/WindowsNoEditor/PythonAPI/Carla_rl/carla')
+
 
 except IndexError:
     pass
@@ -37,8 +37,8 @@ except IndexError:
 
 
 import carla
-
-SECONDS_PER_EPISODE=11.0
+from agents.navigation.local_planner import LocalPlanner
+SECONDS_PER_EPISODE=15.0
 Replay_MEMORY_SIZE =  5000
 MIN_REPLAY_MEMORY_SIZE=1000
 MINI_BATCH_SIZE=16
@@ -51,8 +51,8 @@ MIN_REWARD =-200
 DISCOUNT=0.99
 EPISODES =100
 e=1
-IM_WIDTH=640
-IM_HEIGHT=480
+IM_WIDTH=160
+IM_HEIGHT=120
 e_decay=0.95
 SHOW_METRICS=True
 MODEL_NAME="dqn"
@@ -65,29 +65,31 @@ class CarEnv:
     radar =None
     def __init__(self):
         self.client = carla.Client("localhost", 2000)
-        self.client.set_timeout(30.0)  # duration for errors
+        self.client.set_timeout(80.0)  # duration for errors
         self.world = self.client.get_world()
         self.blueprint_library = self.world.get_blueprint_library()
         self.bp = self.blueprint_library.filter("model3")[0]
-        self.spawn_npc(20,20)
+        
 
 
     def reset(self):
         self.collision_hist=[]
 
-
+      
         self.spawn_point = random.choice(self.world.get_map().get_spawn_points())
         self.vehicle = self.world.spawn_actor(self.bp, self.spawn_point)
-        self.actor_list.append(self.vehicle)
+        actor_list.append(self.vehicle)
+
+        self.spawn_npc(0,0,self.spawn_point.location)
 
         self.cam_bp = self.blueprint_library.find("sensor.camera.rgb")
-        self.cam_bp.set_attribute("image_size_x", f"{self.IM_WIDTH}")
-        self.cam_bp.set_attribute("image_size_y", f"{self.IM_HEIGHT}")
+        self.cam_bp.set_attribute("image_size_x", f"{IM_WIDTH}")
+        self.cam_bp.set_attribute("image_size_y", f"{IM_HEIGHT}")
         self.cam_bp.set_attribute("fov", "110")
 
         spawn_point = carla.Transform(carla.Location(x=2.5, z=0.7))  # question specific spawn spots to place the camera except coordinates
         self.camera_sensor = self.world.spawn_actor(self.cam_bp, spawn_point, attach_to=self.vehicle)
-        self.actor_list.append(self.camera_sensor)
+        actor_list.append(self.camera_sensor)
         # listen to the camera
         self.camera_sensor.listen(lambda data: self.process_img(data))
         # Get the blueprint for the radar sensor
@@ -99,23 +101,18 @@ class CarEnv:
         self.radar_bp.set_attribute('range', '50')
         spawn_point = carla.Transform(carla.Location(x=1.5, y=0.0, z=2.8))
         self.radar_sensor = self.world.spawn_actor(self.radar_bp, spawn_point, attach_to=self.vehicle)
-        self.actor_list.append(self.radar_sensor)
+        actor_list.append(self.radar_sensor)
         # listen to the radar
         self.radar_sensor.listen(lambda data: self.process_radar(data))
-
-        self.vehicle.apply_control(carla.VehicleControl(throttle=0.0 , brake=0.0))
-        time.sleep(4)
         colsensor =self.blueprint_library.find("sensor.other.collision")
         self.colsensor = self.world.spawn_actor(colsensor,spawn_point,attach_to=self.vehicle)
-        self.actor_list.append(self.colsensor)
+        actor_list.append(self.colsensor)
         #check for imapcts
         self.colsensor.listen (lambda impact: self.process_collision(impact))
         while self.front_camera is None:
             time.sleep(0.01)
         self.episode_start =time.time()
-        self.vehicle.apply_control(carla.VehicleControl(throttle=0.0 , brake=0.0))
         return self.front_camera, self.radar
-
 
     def process_collision(self, impact):
         self.collision_hist.append(impact)
@@ -135,20 +132,34 @@ class CarEnv:
         # Get the radar data as a numpy array
         radar_data = np.frombuffer(data.raw_data, dtype=np.dtype('f4'))
         radar_data=radar_data.reshape((radar_data.shape[0]//4,4))
-        if self.Show_metrics:
-            print(radar_data.shape)
+       # if self.Show_metrics:
+           # print(radar_data.shape)
         self.radar=radar_data
 
     def move_walker(self, walker):
-        max = 100
-        target_location = walker.get_location() + carla.Location(x=random.uniform(-max, max),
-                                                                 y=random.uniform(-max, max), z=0.0)
-        walker.set_target_location(target_location)
+        max_distance = 100
+        target_location = walker.get_location() + carla.Location(x=random.uniform(-max_distance, max_distance),
+                                                                y=random.uniform(-max_distance, max_distance), z=0.0)
+        walker_controller_bp = self.blueprint_library.find('controller.ai.walker')
+        walker_controller = self.world.spawn_actor(walker_controller_bp, carla.Transform(), walker)
+        actor_list.append(walker_controller)
+        self.walker_controller_list.append(walker_controller)
+        walker_controller.start()
+        walker_controller.go_to_location(target_location)
 
-    def spawn_npc(self, number_of_vehicles, number_of_walkers):
+
+    def generate_spawn_point_near_location(self, location, radius):
+        spawn_points = self.world.get_map().get_spawn_points()
+        spawn_points_near_location = [sp for sp in spawn_points if sp.location.distance(location) <= radius]
+        if not spawn_points_near_location:
+            return random.choice(spawn_points)
+        return random.choice(spawn_points_near_location)
+
+    def spawn_npc(self, number_of_vehicles, number_of_walkers,main_actor_location):
         # Get the blueprint for all vehicles and all walkers
         self.walker_blueprints = self.blueprint_library.filter('walker.pedestrian.*')
         self.vehicle_blueprints =self.blueprint_library.filter('vehicle.*.*')
+        
         # Spawn the vehicles
         for i in range(number_of_vehicles):
             # Choose a random blueprint for the vehicle
@@ -156,22 +167,30 @@ class CarEnv:
             npc = None
             self.bp = random.choice(self.vehicle_blueprints)
             while npc is None:
-                spawn_point = random.choice(self.world.get_map().get_spawn_points())
+                spawn_point = self.generate_spawn_point_near_location(main_actor_location, radius=100)
                 npc = self.world.try_spawn_actor(self.bp, spawn_point)
             npc.set_autopilot(True)
             print('created %s' % npc.type_id)
-            self.actor_list.append(npc)
+            actor_list.append(npc)
         print('cars done')
         # Spawn the pedestrians
+        self.walker_controller_list = []
+        max_attempts = 10  # Maximum number of attempts for each pedestrian
         for i in range(number_of_walkers):
             npc2 = None
             bp2 = random.choice(self.walker_blueprints)
-            while npc is None:
-                spawn_point = random.choice(self.world.get_map().get_spawn_points())
+            for _ in range(max_attempts):
+                spawn_point = self.generate_spawn_point_near_location(main_actor_location, radius=100)
                 npc2 = self.world.try_spawn_actor(bp2, spawn_point)
-                # move_walker(npc)
-            print('created walker')
-            self.actor_list.append(npc2)
+                if npc2 is not None:
+                    break
+            if npc2 is None:
+                print(f"Failed to spawn walker {i + 1} after {max_attempts} attempts")
+                continue
+            self.move_walker(npc2)
+            print(f"Created walker {i + 1}")
+            actor_list.append(npc2)
+        
 
     def step(self, function):
         if(function==0):
@@ -180,11 +199,19 @@ class CarEnv:
             self.vehicle.apply_control(carla.VehicleControl(throttle=0.5, brake=0.0))
         elif (function == 2):
             self.vehicle.apply_control(carla.VehicleControl(throttle=0.8, brake=0.0))
+        elif (function == 2):
+            self.vehicle.apply_control(carla.VehicleControl(throttle=0.1, brake=0.0))
+        elif (function == 2):
+            self.vehicle.apply_control(carla.VehicleControl(throttle=0.8, brake=0.0))
+        elif (function == 2):
+            self.vehicle.apply_control(carla.VehicleControl(throttle=0.2, brake=0.0))
         elif (function == 3):
-            self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=0.3))
+            self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=1.0))
         elif (function == 4):
             self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=1.0))
-        velocity= self.vehicle.get_velocity
+
+        velocity= self.vehicle.get_velocity()
+        done= False
         kmh= int(3.6 * math.sqrt(velocity.x**2 + velocity.y**2 + velocity.z**2))
         if (len(self.collision_hist) != 0):
             done =True
@@ -196,246 +223,32 @@ class CarEnv:
             done = False
             reward=1
         if self.episode_start + SECONDS_PER_EPISODE < time.time():
+            print("time is up")
             done=True
         return self.front_camera ,reward, done
 
-class DQNAgent:
-    def __init__(self):
-        self.learning_rate= 0.001
-        self.model =self.create_model()
-        self.target_model= self.create_model()
-        self.target_model.set_weights(self.model.get_weights())
-        self.replay_memory = deque(maxlen=Replay_MEMORY_SIZE)
-        self.tensorboard =  self.tensorboard = ModifiedTensorBoard(log_dir=f"logs/{MODEL_NAME}-{int(time.time())}")
-        self.target_update_counter =0
-        self.graph = tf.compat.v1.get_default_graph()
-
-
-        #flags
-        self.terminate= False
-        self.last_logged_episode =0
-        self.training_initialized =False
-
-    def create_model(self):
-        model  = Sequential()
-        model.add(Conv2D(256,(3,3), input_shape=(IM_HEIGHT,IM_WIDTH,3 )))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(2,2))
-        model.add(Dropout(0.2))
-        model.add(Conv2D(256, (3, 3)))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(2, 2))
-        model.add(Dropout(0.2))
-        model.add(Flatten())
-        model.add(Dense(64))
-        model.add(Dense(2, Activation("linear")))
-        model.compile(loss="mse",
-                      optimizer=Adam(learning_rate=self.learning_rate), metrics=['accuracy'])
-        return model
-
-    def update_replay_memory(self, transition):
-        self.replay_memory.append(transition)
-
-    def get_qs(self, state):
-        return self.model.predict(np.array(state).reshape(-1, *state.shape) / 255)[0]
-
-
-    def train(self):
-        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
-            return
-
-        minibatch=random.sample(self.replay_memory, MINI_BATCH_SIZE)
-
-        current_states = np.array([transition[0] for transition in minibatch])/255
-        with self.graph.as_default():
-            current_qs_list = self.model.predict(current_states, PREDICTION_BATCH_SIZE)
-
-        new_current_states = np.array([transition[3] for transition in minibatch]) / 255
-        with self.graph.as_default():
-            future_qs_list = self.model.predict(new_current_states, PREDICTION_BATCH_SIZE)
-
-        X = []
-        y = []
-
-        for index, (current_state, action, reward, new_state, done) in enumerate(minibatch):
-            if not done:
-                max_future_q = np.max(future_qs_list[index])
-                new_q = reward + DISCOUNT * max_future_q
-            else:
-                new_q = reward
-
-            current_qs = current_qs_list[index]
-            current_qs[action] = new_q
-
-            X.append(current_state)
-            y.append(current_qs)
-
-            log_this_step = False
-            if self.tensorboard.step > self.last_logged_episode:
-                log_this_step = True
-                self.last_log_episode = self.tensorboard.step
-
-            with self.graph.as_default():
-                self.model.fit(np.array(X) / 255, np.array(y), batch_size=TRAINING_BATCH_SIZE, verbose=0, shuffle=False,
-                               callbacks=[self.tensorboard] if log_this_step else None)
-            if log_this_step:
-                self.target_update_counter += 1
-
-            if self.target_update_counter > UPDATE_TARGET_STEPS:
-                self.target_model.set_weights(self.model.get_weights())
-                self.target_update_counter = 0
-
-    def train_in_loop(self):
-        X = np.random.uniform(size=(1, IM_HEIGHT, IM_WIDTH, 3)).astype(np.float32)
-        y = np.random.uniform(size=(1, 2)).astype(np.float32)
-        with self.graph.as_default():
-            self.model.fit(X, y, verbose=False, batch_size=1)
-
-        self.training_initialized = True
-
-        while True:
-            if self.terminate:
-                return
-            self.train()
-            time.sleep(0.01)
-
-
-# Own Tensorboard class
-class ModifiedTensorBoard(TensorBoard):
-
-    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.step = 1
-        self.writer = tf.summary.create_file_writer(self.log_dir)
-
-    # Overriding this method to stop creating default log writer
-    def set_model(self, model):
-        pass
-
-    # Overrided, saves logs with our step number
-    # (otherwise every .fit() will start writing from 0th step)
-    def on_epoch_end(self, epoch, logs=None):
-        self.update_stats(**logs)
-
-    # Overrided
-    # We train for one batch only, no need to save anything at epoch end
-    def on_batch_end(self, batch, logs=None):
-        pass
-
-    # Overrided, so won't close writer
-    def on_train_end(self, _):
-        pass
-
-    # Custom method for saving own metrics
-    # Creates writer, writes custom metrics and closes writer
-    def update_stats(self, **stats):
-        self._write_logs(stats, self.step)
-
-
+def destroy_all_actors():
+    global actor_list
+    print(" initialize destroy")
+    for actor in actor_list:
+        if actor is not None:
+            actor.destroy()
+    print("destroyed")
+    actor_list = []
 
 if __name__ == '__main__':
-    tf.compat.v1.disable_eager_execution()
-    FPS =20
-    ep_rewards =[-200]
-    np.random.seed(1)
-    tf.random.set_seed(1)
-    gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=MEMORY_FRACTION)
-    config = tf.compat.v1.ConfigProto(gpu_options=gpu_options)
-    sess = tf.compat.v1.Session(config=config)
-    tf.compat.v1.keras.backend.set_session(sess)
-    if not os.path.isdir("models"):
-        os.makedirs("models")
-
-    agent =DQNAgent()
-    print("check dqn agent init")
-    env= CarEnv()
-    print("check CarEnv init")
-    # Start training thread and wait for training to be initialized
-    trainer_thread = Thread(target=agent.train_in_loop, daemon=True)
-    trainer_thread.start()
-    print("gonna initialize train")
-    while not agent.training_initialized:
-        time.sleep(0.01)
-        print("init training")
-    print("init trained")
-
-    # Initialize predictions - forst prediction takes longer as of initialization that has to be done
-    # It's better to do a first prediction then before we start iterating over episode steps
-    agent.get_qs(np.ones((IM_HEIGHT,IM_WIDTH, 3)))
-    print("gonna  train")
-    # Iterate over episodes
-    for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
-        # try:
-
-        env.collision_hist = []
-
-        # Update tensorboard step every episode
-        agent.tensorboard.step = episode
-
-        # Restarting episode - reset episode reward and step number
-        episode_reward = 0
-        step = 1
-
-        # Reset environment and get initial state
-        current_state = env.reset()
-
-        # Reset flag and start iterating until episode ends
+    env = CarEnv()
+    num_episodes = 10
+    for episode in range(num_episodes):
+        print(f"Episode: {episode + 1}")  # Debug: Print the current episode
+        env.reset()
         done = False
-        episode_start = time.time()
+        step_count = 0
+        while not done:
+            action = 2
+            _, reward, done = env.step(action)
+            step_count += 1
+            print(f"Step: {step_count}, Reward: {reward}, Done: {done}")
+            time.sleep(0.1)
+        destroy_all_actors()
 
-        # Play for given number of seconds only
-        while True:
-
-            # This part stays mostly the same, the change is to query a model for Q values
-            if np.random.random() > e:
-                # Get action from Q table
-                action = np.argmax(agent.get_qs(current_state))
-            else:
-                # Get random action
-                action = np.random.randint(0, 3)
-                # This takes no time, so we add a delay matching 60 FPS (prediction above takes longer)
-                time.sleep(1 / FPS)
-
-            new_state, reward, done, _ = env.step(action)
-
-            # Transform new continous state to new discrete state and count reward
-            episode_reward += reward
-
-            # Every step we update replay memory
-            agent.update_replay_memory((current_state, action, reward, new_state, done))
-
-            current_state = new_state
-            step += 1
-
-            if done:
-                break
-
-        # End of episode - destroy agents
-        for actor in env.actor_list:
-            actor.destroy()
-
-        # Append episode reward to a list and log stats (every given number of episodes)
-        ep_rewards.append(episode_reward)
-        if not episode % AGGREGATE_STATS_EVERY or episode == 1:
-            average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:]) / len(ep_rewards[-AGGREGATE_STATS_EVERY:])
-            min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
-            max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
-            agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward,
-                                           epsilon=e)
-
-            # Save model, but only when min reward is greater or equal a set value
-            if min_reward >= MIN_REWARD:
-                agent.model.save(
-                    f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
-
-        # Decay epsilon
-        if e > MIN_EPSILON:
-            e *= EPSILON_DECAY
-            e= max(MIN_EPSILON, e)
-
-    # Set termination flag for training thread and wait for it to finish
-    agent.terminate = True
-    trainer_thread.join()
-    agent.model.save(
-        f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
